3d3e8fa890918b3172db99f2f33381feb731522a
Drey <oluwadamilareadeniye@yahoo.com>
2025-08-11 04:18:53 +0100

    added admin to the project

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from .models import Author, Category, Publisher, Tag, Book, BookImage
 
-# Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("name", "date_of_birth", "created_at")
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "created_at")
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ("name", "website", "created_at")
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "created_at")
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+class BookImageInline(admin.TabularInline):
+    model = BookImage
+    extra = 1
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "isbn", "price", "stock_quantity", "category", "publication_date", "created_at")
+    list_filter = ("category", "publisher", "authors", "tags")
+    search_fields = ("title", "isbn")
+    prepopulated_fields = {"slug": ("title",)}
+    inlines = [BookImageInline]
+    ordering = ("title",)
+
+
+@admin.register(BookImage)
+class BookImageAdmin(admin.ModelAdmin):
+    list_display = ("book", "is_main", "created_at")
+    list_filter = ("is_main",)
